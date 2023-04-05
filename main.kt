a2b8356b2032f1f264f52fb7a0563864f93af61d
Ramez Nasser <97125244+RamezNasser5@users.noreply.github.com>
2023-04-05 23:02:22 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,97 +1,66 @@
-class Chess {
+fun main() {
+    //object chess from class Chess use to treat with chess form
+    val chess = Chess()
+    println("Pawns-Only Chess")
+    println("First Player's name:")
 
-    //this a mutable list to store a strings to draw chess form
-    private var rows: MutableList<String>
-    private var checkValidation: Boolean
+    //the name of first player
+    val firstName = readln()
+    println("Second Player's name:")
 
-    //this constructor to initialize the private mutable list rows
-    init {
-        rows = mutableListOf(
-            "    a   b   c   d   e   f   g   h  ","1 |   |   |   |   |   |   |   |   |",
-            "2 | W | W | W | W | W | W | W | W |","3 |   |   |   |   |   |   |   |   |",
-            "4 |   |   |   |   |   |   |   |   |","5 |   |   |   |   |   |   |   |   |",
-            "6 |   |   |   |   |   |   |   |   |","7 | B | B | B | B | B | B | B | B |",
-            "8 |   |   |   |   |   |   |   |   |"
-        )
-        checkValidation = true
-    }
+    //the name of second player
+    val secondName = readln()
+    chess.drawChess()
 
-    //function drawChess use to draw the shape of chess
-    fun drawChess()
-    {
-        for (i in 8 downTo 0)
-        {
-            println("  +---+---+---+---+---+---+---+---+")
-            println(rows[i])
-        }
-    }
+    //checkPlayer use to switch between two players
+    var checkPlayer = 0
 
-    //accessToPiece function use to reach to the piece we want to move it
-    private fun accessToPiece(expression: String,beforeMove: Int, afterMove: Int, indexOfPiece: Int): Boolean {
-        if (this.rows[beforeMove][indexOfPiece] == ' ')
-        {
-            println("No black pawn at ${expression[0]}$beforeMove")
-            checkValidation = false
-        }
-        else {
-            checkValidation = true
-            this.rows[afterMove] = """${
-                this.rows[afterMove].substring(
-                    0,
-                    indexOfPiece
-                )
-            }${this.rows[beforeMove][indexOfPiece]}${this.rows[afterMove].substring(indexOfPiece + 1)}""".trimMargin()
-            this.rows[beforeMove] = """${
-                this.rows[beforeMove].substring(
-                    0,
-                    indexOfPiece
-                )
-            } ${this.rows[beforeMove].substring(indexOfPiece + 1)}""".trimMargin()
-        }
-        return checkValidation
-    }
+    //firstPlayerMove variable is the first player move
+    var firstPlayerMove: String
 
-    //function pieceMoving use to move on piece to another in chess game
-    fun pieceMoving(expression: String): Boolean {
+    //secondPlayerMove variable is the second player move
+    var secondPlayerMove: String
 
-        //beforeMove is a variable store a row number to move a piece from it to afterMove row
-        val beforeMove = expression[1].code -48
-        val afterMove = expression[3].code-48
+    //regex variable use to check the validation of player move
+    var regex: Regex
+    while (true) {
+        if (checkPlayer % 2 == 0) {
+            println(message = "$firstName's turn:")
 
-        //when statement use to access a piece in beforeMove row and move it to afterMove row
-        when(expression[0]) {
-            'a' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,4)
+            firstPlayerMove = readln()
+            if (firstPlayerMove == "exit") {
+                println("Bye!")
+                break
+            } else {
+                regex = Regex("[a-h][1-8][a-h][1-8]")
+                if (regex.matches(firstPlayerMove)) {
+                    if (chess.pieceMoving(firstPlayerMove,checkPlayer)) {
+                        checkPlayer++
+                        chess.drawChess()
+                    }
+                } else {
+                    println("Invalid Input")
+                }
             }
+        } else {
+            println("$secondName's turn:")
 
-            'b' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,8)
-            }
-
-            'c' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,12)
-            }
-
-            'd' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,16)
-            }
-
-            'e' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,20)
-            }
-
-            'f' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,24)
-            }
-
-            'g' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,28)
-            }
-
-            'h' -> {
-                checkValidation = accessToPiece(expression,beforeMove,afterMove,32)
+            //secondPlayerMove variable is the second player move
+            secondPlayerMove = readln()
+            if (secondPlayerMove == "exit") {
+                println("Bye!")
+                break
+            } else {
+                regex = Regex("[a-h][1-8][a-h][1-8]")
+                if (regex.matches(secondPlayerMove)) {
+                    if (chess.pieceMoving(secondPlayerMove,checkPlayer)) {
+                        checkPlayer++
+                        chess.drawChess()
+                    }
+                } else {
+                    println("Invalid Input")
+                }
             }
         }
-        return checkValidation
     }
 }
